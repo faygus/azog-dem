d52e17ef6839745ecdf8543f667faa660cccdce6
DURIER Jean-Samuel <durier_j@etna-alternance.net>
2019-05-05 18:13:15 +0200

    Updates

---
+++ precache-manifest.d329034912abb1c46f105a99b0e3e12c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22459f18b726556c6e104bdca86799e",
+    "url": "/azog-demo/index.html"
+  },
+  {
+    "revision": "82722d70cdb299b23a6e",
+    "url": "/azog-demo/static/css/2.bd9a19c3.chunk.css"
+  },
+  {
+    "revision": "ff3fc5b187fbb4eda0e3",
+    "url": "/azog-demo/static/css/main.a81ff926.chunk.css"
+  },
+  {
+    "revision": "82722d70cdb299b23a6e",
+    "url": "/azog-demo/static/js/2.28ae1d79.chunk.js"
+  },
+  {
+    "revision": "ff3fc5b187fbb4eda0e3",
+    "url": "/azog-demo/static/js/main.5ca8c5c6.chunk.js"
+  },
+  {
+    "revision": "583b9cb8107dad799f74",
+    "url": "/azog-demo/static/js/runtime~main.56e032a2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/azog-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/azog-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/azog-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/azog-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/azog-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
